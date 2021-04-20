ed610aa7f68f94c045a92866bcd1e16435453d54
shizuka12 <flowerflowersilent@gmail.com>
2021-04-20 14:00:33 +0900

    made signup function

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+# from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = SignUpForm()
+
+    return render(request, 'accounts/signup.html', {'form':form})
