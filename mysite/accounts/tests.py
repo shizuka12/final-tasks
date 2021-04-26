c71b76ab394e58c02eb124428bca16c37e777c03
shizuka12 <flowerflowersilent@gmail.com>
2021-04-26 13:05:06 +0900

    modified spaces of indent

--- mysite/accounts/tests.py
+++ mysite/accounts/tests.py
@@ -9,33 +9,33 @@ username = "already_exist"
 username2 = "new_username"
 
 class Signup_Tests(TestCase):
-  
-  def setUp(self):
-    user = User.objects.create_user(username, '', 'pass_a')
-    user.save()
+    
+    def setUp(self):
+        user = User.objects.create_user(username, '', 'pass_a')
+        user.save()
 
-  '''
-  もしUserCreationFormのusernameがまだ登録されていないもので、
-  password1とpassword2が一致し、かつ条件を満たした文字列であれば,
-  form.is_valid()はTrueになる 
-  '''
-  def test_newusername_and_correct_password(self): 
-    form = UserCreationForm({'username': username2, 'password1': 'pass_b', 'password2': 'pass_b'})
-    self.assertTrue(form.is_valid)
+    '''
+    もしUserCreationFormのusernameがまだ登録されていないもので、
+    password1とpassword2が一致し、かつ条件を満たした文字列であれば,
+    form.is_valid()はTrueになる 
+    '''
+    def test_newusername_and_correct_password(self): 
+        form = UserCreationForm({'username': username2, 'password1': 'pass_b', 'password2': 'pass_b'})
+        self.assertTrue(form.is_valid)
 
-  '''
-  もしUserCreationFormのusernameが既に登録されたものであれば,
-  form.is_valid()はFalseになる
-  '''
-  def test_already_existed_name(self):
-    form = UserCreationForm({'username': username, 'password1': 'pass_b', 'password2': 'pass_b'})
-    self.assertFalse(form.is_valid())
+    '''
+    もしUserCreationFormのusernameが既に登録されたものであれば,
+    form.is_valid()はFalseになる
+    '''
+    def test_already_existed_name(self):
+        form = UserCreationForm({'username': username, 'password1': 'pass_b', 'password2': 'pass_b'})
+        self.assertFalse(form.is_valid())
 
-  '''
-  もしUserCreationFormのusernameが登録されていないものだとしても、
-  password1とpassword2が一致しなければform.is_valid()はFalseになる
-  '''
-  def test_with_dismatch_password(self):
-    form = UserCreationForm({'username': username2, 'password1': 'pass_b', 'password2': 'pass_c'})
-    self.assertFalse(form.is_valid())
+    '''
+    もしUserCreationFormのusernameが登録されていないものだとしても、
+    password1とpassword2が一致しなければform.is_valid()はFalseになる
+    '''
+    def test_with_dismatch_password(self):
+        form = UserCreationForm({'username': username2, 'password1': 'pass_b', 'password2': 'pass_c'})
+        self.assertFalse(form.is_valid())
 
