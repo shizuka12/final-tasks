7b74dd014fbf46c3dc758eaeb38efa26a2c06d2b
shizuka12 <flowerflowersilent@gmail.com>
2021-05-10 15:04:23 +0900

    modified TopViewTests

--- mysite/tmitter/tests.py
+++ mysite/tmitter/tests.py
@@ -1,39 +1,35 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from django.contrib.auth import login
 from .models import Tmeet
 from django.urls import reverse
 
 # Create your tests here.
-
-data1 = {
-    'username': 'username1',
-    'password': 'password_1'
-}
-data2 = {
-    'username': 'username2',
-    'password': 'password_2'
-}
-
 class TopViewTests(TestCase):
     def setUp(self):
         self.timeline_list = []
-        for i in range(1,3):
-            data = 'data{}'.format(str(i))
-            user = User.objects.create_user(data)
-            self.client.login(data=data)
-            content = 'This is a timeline test by username' + str(i)
-            Tmeet.objects.create(author_id=i, content=content)
-            self.timeline_list.append(content)
+        self.user = User.objects.create_user('username1', '', 'password_1')
+        self.client = Client()
+        self.client.login(username='username1', password='password_1')
+        content = 'This is a timeline test by username1'
+        Tmeet.objects.create(author_id=1, content=content)
+        self.timeline_list.append(content)
+        self.user = User.objects.create_user('username2', '', 'password_2')
+        self.client.login(username='username2', password='password_2')
+        content = 'This is a timeline test by username2'
+        Tmeet.objects.create(author_id=2, content=content)
+        self.timeline_list.append(content)
+        self.timeline_list.reverse()
     
     def test_of_timeline(self):
         '''
         トップページにアクセスしたら、
-        すべてのユーザーのツミートが表示される
+        すべてのユーザーのツミートが新しい順に表示される
         '''
-        self.client.login(data=data2)
-        print(reverse('tmitter:top', args=str(2)))
+        # print(self.user.username)
+        # print(self.client.get(reverse('tmitter:top', args=str(2))))
         response = self.client.get(reverse('tmitter:top', args=str(2)))
         queryset = response.context['tmeet_list']
+        # print(queryset)
         for i in range(2):
             self.assertEqual(queryset[i].content, self.timeline_list[i])
