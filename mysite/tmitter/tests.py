509ad43d033ecff4d0e87c425a3b3161df5a69be
shizuka12 <flowerflowersilent@gmail.com>
2021-05-11 13:45:18 +0900

    added AccountpageViewTests

--- mysite/tmitter/tests.py
+++ mysite/tmitter/tests.py
@@ -30,3 +30,33 @@ class TopViewTests(TestCase):
         queryset = response.context['tmeet_list']
         for i in range(2):
             self.assertEqual(queryset[i].content, self.timeline_list[i])
+
+
+class AccountpageViewTests(TestCase):
+    def setUp(self):
+        self.accountpage_list = []
+        self.user = User.objects.create_user('username1', '', 'password_1')
+        self.client = Client()
+        self.client.login(username='username1', password='password_1')
+        for i in range(2):
+            content = 'This is a accountpage test' +str(i+1)+ ' by username1'
+            Tmeet.objects.create(author_id=1, content=content)
+        self.user = User.objects.create_user('username2', '', 'password_2')
+        self.client.login(username='username2', password='password_2')
+        for i in range(2):
+            content = 'This is a accountpage test' +str(i+1)+ ' by username2'
+            Tmeet.objects.create(author_id=2, content=content)
+            self.accountpage_list.append(content)
+        self.accountpage_list.reverse()
+        print(self.accountpage_list)
+
+    def test_of_tmeetlist(self):
+        '''
+        アカウントページにアクセスしたら、
+        そのユーザーのツミートが新しい順に表示される
+        '''
+        response = self.client.get(reverse('tmitter:accountpage', args=str(2)))
+        queryset = response.context['tmeet_list']
+        print(queryset)
+        for i in range(2):
+            self.assertEqual(queryset[i].content, self.accountpage_list[i])
