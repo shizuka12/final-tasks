6e508db2489b5d13ad768f738f56fe5dc8e78e70
shizuka12 <flowerflowersilent@gmail.com>
2021-05-14 22:44:09 +0900

    modified tests

--- mysite/accounts/tests.py
+++ mysite/accounts/tests.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.urls import reverse
-from django.contrib.auth import login, logout
 
 # Create your tests here.
 
--- mysite/tmitter/tests.py
+++ mysite/tmitter/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
-from django.contrib.auth import login
 from .models import Tmeet
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
@@ -11,7 +10,6 @@ class TopViewTests(TestCase):
     def setUp(self):
         self.timeline_list = []
         self.user = User.objects.create_user('username1', '', 'password_1')
-        self.client = Client()
         self.client.login(username='username1', password='password_1')
         content = 'This is a timeline test by username1'
         Tmeet.objects.create(author_id=1, content=content)
@@ -38,7 +36,6 @@ class AccountpageViewTests(TestCase):
     def setUp(self):
         self.accountpage_list = []
         self.user = User.objects.create_user('username1', '', 'password_1')
-        self.client = Client()
         self.client.login(username='username1', password='password_1')
         for i in range(2):
             content = 'This is an accountpage test' +str(i+1)+ ' by username1'
@@ -66,26 +63,23 @@ class AccountpageViewTests(TestCase):
 
 class DeleteViewTests(TestCase):
     def setUp(self):
-        self.accountpage_list = []
+        self.deltest_list = []
         self.user = User.objects.create_user('username1', '', 'password_1')
-        self.client = Client()
         self.client.login(username='username1', password='password_1')
         for i in range(3):
             content = 'This is a deleteing funtction test' +str(i+1)+ ' by username1'
             Tmeet.objects.create(author_id=1, content=content)
-            self.accountpage_list.append(content)
+            self.deltest_list.append(content)
             time.sleep(0.1)
-        self.accountpage_list.reverse()
+        self.deltest_list.reverse()
 
     def test_of_delete(self):
         '''
-        ツミートを削除したらデータベースから削除され、
-        アカウントページに表示されなくなる
+        ツミートを削除したらデータベースから削除される
         '''
-        response = self.client.get(reverse('tmitter:accountpage', args=str(1)))
         self.tmeet = get_object_or_404(Tmeet, pk=3)
         self.tmeet.delete()
         queryset = Tmeet.objects.filter(author=1).order_by('-tmeeted_date')
-        self.accountpage_list.pop(0)
+        self.deltest_list.pop(0)
         for i in range(2):
-            self.assertEqual(queryset[i].content, self.accountpage_list[i])
+            self.assertEqual(queryset[i].content, self.deltest_list[i])
