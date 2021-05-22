240230f1114782402f1bdda4c381cf49509549fe
shizuka12 <flowerflowersilent@gmail.com>
2021-05-22 19:27:32 +0900

    added model of Follow

--- mysite/tmitter/models.py
+++ mysite/tmitter/models.py
@@ -10,3 +10,11 @@ class Tmeet(models.Model):
 
     def __str__(self):
         return self.content
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, related_name='follower', on_delete=models.CASCADE)
+    following = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
+    followed_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{self.follower} follows {self.following}"
