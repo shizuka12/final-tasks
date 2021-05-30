afe508b8b1dcdfc138a22f6b9c441eaa3dddaecf
shizuka12 <flowerflowersilent@gmail.com>
2021-05-30 12:04:23 +0900

    deleted unnecessary module

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
